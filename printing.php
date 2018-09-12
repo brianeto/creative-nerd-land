489cfd8db40bb4888ab19e457602cf447d68f1a8
Brianeto <bdmb45@gmail.com>
2018-09-12 12:03:06 -0500

    forms

--- printing.php
+++ printing.php
@@ -80,7 +80,7 @@
 		</div>
 	</div>
 </section>
-<section id="seccion_diseño_web_iconos" class="seccion_padding bg-imagen opacity opacity-green">
+<section id="seccion_diseño_web_iconos" class="seccion_padding diagonal_cut bg-imagen opacity opacity-green">
 		<div id="div_contenedor_iconos">
 			<div class="row justify-content-center text-center">
 				<div class="grid-icon -icon col-2 btn" data-toggle="modal" data-target="#tarjetasPresentacionModal">
@@ -206,6 +206,40 @@
 		</div>
 	</div>
 </div>
+<section id="seccion_contacto" class="seccion_padding">
+	<div class="container">
+		<p class="tag_subtitulo_secciones tag_titulo_secciones_black text-center">
+			¿Estás listo para iniciar tu proyecto?<br>
+			<span>Cuéntanos de él</span>
+		</p>
+		<div class="row">
+			<div class="col-12 col-lg-6 align-self-center">
+				<form class="text-center" id="form_contacto" method="post" action="javascript:enviarContacto();">
+					<div class="form-row">
+						<div class="col-12 col-lg-6 text-left">
+							<label for="txt_nombre" class="tag_indicador_form">Nombre</label>
+							<input type="text" class="form-control form-control-mine" id="txt_nombre">
+						</div>
+						<div class="col-12 col-lg-6 text-left">
+							<label for="txt_email" class="tag_indicador_form">Email</label>
+							<input type="email" class="form-control form-control-mine" id="txt_email">
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="col-12 text-left">
+							<label for="txt_mensaje" class="tag_indicador_form">Mensaje</label>
+							<textarea class="form-control form-control-mine" id="txt_mensaje"></textarea>
+						</div>
+					</div>
+					<button type="submit" class="btn btn-form-mine" id="btn_enviar">ENVIAR</button>
+				</form>
+			</div>
+			<div class="col-12 col-lg-6 align-self-end man">
+				<img class="img_persona_contacto" src="<?php echo $path; ?>image/contact.png">
+			</div>			
+		</div>		
+	</div>		
+</section>
 <?php 
 	include('footer.php');
 ?>
